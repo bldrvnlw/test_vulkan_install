9ee0f339daf4216f301203c3eb387600d9e06067
Baldur van Lew <b.van_lew@lumc.nl>
2025-09-12 17:34:28 +0200

    Demonstrate switching from kompute to not kompute build with USE_VULKAN_KOMPUTE

--- src/main.cpp
+++ src/main.cpp
@@ -2,21 +2,27 @@
 #include <iostream>
 #include <memory>
 #include <vector>
-
-#include <kompute/Kompute.hpp>
+#include "testapp_config.h"
+#ifdef USE_VULKAN_KOMPUTE  
+    #include <kompute/Kompute.hpp>
+#endif
 #include <shader.hpp>
 
 int
 main()
 {
+  auto A = std::vector<float>{ 2.0, 4.0, 6.0 };
+  auto B = std::vector<float>{ 0.0, 1.0, 2.0 };
+  auto C = std::vector<float>(3, 0.0);
+#ifdef USE_VULKAN_KOMPUTE    
     kp::Manager mgr;
 
     std::shared_ptr<kp::TensorT<float>> tensorInA =
-      mgr.tensor({ 2.0, 4.0, 6.0 });
+      mgr.tensor(A);
     std::shared_ptr<kp::TensorT<float>> tensorInB =
-      mgr.tensor({ 0.0, 1.0, 2.0 });
+      mgr.tensor(B);
     std::shared_ptr<kp::TensorT<float>> tensorOut =
-      mgr.tensor({ 0.0, 0.0, 0.0 });
+      mgr.tensor(C);
 
     const std::vector<std::shared_ptr<kp::Tensor>> params = { tensorInA,
                                                               tensorInB,
@@ -33,7 +39,7 @@ main()
       ->eval();
 
     // prints "Output {  0  4  12  }"
-    std::cout << "Output: {  ";
+    std::cout << "GPU Vulkan Output: {  ";
     for (const float& elem : tensorOut->vector()) {
         std::cout << elem << "  ";
     }
@@ -42,4 +48,16 @@ main()
     if (tensorOut->vector() != std::vector<float>{ 0, 4, 12 }) {
         throw std::runtime_error("Result does not match");
     }
+#else
+    std::cout << "Vulkan kompute not enabled" << std::endl;
+    for (size_t i = 0; i < A.size(); i++) {
+        C[i] = A[i] * B[i];
+    }
+    std::cout << "CPU Output: {  ";
+    for (const float& elem : C) {
+        std::cout << elem << "  ";
+    }
+    std::cout << "}" << std::endl;  
+#endif
+    return 0;
 }
